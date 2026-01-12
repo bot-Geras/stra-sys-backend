559074d7ff08c2468fb149cfb8567e2076a659f0
Brian David <briandave771@gmail.com>
2026-01-13 00:22:13 +0300

    Initialize this App project

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {PORT} from './config/env.js';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcomr to a Smart Triage System!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
